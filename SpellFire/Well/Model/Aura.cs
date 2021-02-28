2f16d2d9b88f122c8b00c930ffcaa2f6fd6ebbcf
KKovs <krzysztof.kowalski.kontakt@gmail.com>
2021-02-28 15:58:43 +0100

    Fix reading aura stacks

--- SpellFire/Well/Model/Aura.cs
+++ SpellFire/Well/Model/Aura.cs
@@ -7,14 +7,15 @@ using System.Threading.Tasks;
 
 namespace SpellFire.Well.Model
 {
-	[StructLayout(LayoutKind.Sequential)]
+	[StructLayout(LayoutKind.Sequential, Pack = 1)]
 	public struct Aura
 	{
 		public Int64 creatorGuid;
 		public Int32 auraID;
 		public byte flags;
 		public byte level;
-		public Int16 stackCount;
+		public byte stackCount;
+		public byte unknown1;
 		public UInt32 duration;
 		public UInt32 endTime;
 	}
